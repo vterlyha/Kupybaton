8df456e5d132335dfd9edce5c54280765d4351f1
mkhrun <mkhrun@intellias.com>
2016-01-23 14:49:38 +0200

    Fix retrieving selected product in servlet

--- src/com/kupybaton/web/servlets/PurchasePage.java
+++ src/com/kupybaton/web/servlets/PurchasePage.java
@@ -1,25 +1,25 @@
 package com.kupybaton.web.servlets;
 
-import java.io.IOException;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.kupybaton.model.Product;
 import com.kupybaton.model.ProductList;
 import com.kupybaton.web.jdbc.AllProductSelectExpert;
 import com.kupybaton.web.jdbc.GetLastCreatedList;
 import com.kupybaton.web.jdbc.PurchaseExpert;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PurchasePage extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		
+
 		String listname = request.getParameter("listname");
-						
+
 		if (listname.isEmpty()) {
 			response.setContentType("text/html");
 			String warningMessage = "You have to type the name of list!!!";
@@ -27,7 +27,7 @@ public class PurchasePage extends HttpServlet {
 			RequestDispatcher view = request.getRequestDispatcher("WEB-INF/jsp/CreateNewList.jsp");
 			view.forward(request, response);
 		}
-		
+
 		if (listname.length() > 0) {
 			response.setContentType("text/html");
 			PurchaseExpert pe = new PurchaseExpert();
@@ -42,4 +42,50 @@ public class PurchasePage extends HttpServlet {
 			view.forward(request, response);
 		}
 	}
+
+    /*
+     Added just for test.
+     But it would be great to have doGet method (for example when edit purchases)
+      */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        List products = new ArrayList() {
+            {
+                add(new Product() {
+                    {
+                        setName("banana");
+                        setId(1);
+                    }
+                });
+                add(new Product() {
+                    {
+                        setName("kiwi");
+                        setId(2);
+                    }
+                });
+                add(new Product() {
+                    {
+                        setName("mango");
+                        setId(3);
+                    }
+                });
+                add(new Product() {
+                    {
+                        setName("apple");
+                        setId(4);
+                    }
+                });
+                add(new Product() {
+                    {
+                        setName("ananas");
+                        setId(5);
+                    }
+                });
+            }
+        };
+        request.setAttribute("products", products);
+        RequestDispatcher view = request.getRequestDispatcher("WEB-INF/jsp/PurchasePage.jsp");
+        view.forward(request, response);
+    }
 }
--- webapps/WEB-INF/jsp/PurchasePage.jsp
+++ webapps/WEB-INF/jsp/PurchasePage.jsp
@@ -1,6 +1,3 @@
-<%@ page import="com.kupybaton.model.Product"%>
-<%@ page import="java.util.List"%>
-<%@ page import="com.kupybaton.model.ProductList"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt"%>
 <html>
 <head>
@@ -8,26 +5,26 @@
 </head>
 <body>
 	<form method="post" action="AddFirstProduct.do">
-	
-	<h1 >
-		<c:forEach items="${productlist}" var="list">
-			<c:set var="listId" scope="request" value="${list.getId()}"/> 
-			<c:out value="${list.getName()}" />
-		</c:forEach>
-	</h1>
+        <h1>
+            <c:forEach items="${productlist}" var="list">
+                <c:set var="listId" scope="request" value="${list.getId()}"/>
+                <c:out value="${list.getName()}" />
+            </c:forEach>
+        </h1>
 
-	<select name="productName" id="mySelect">
-		<c:forEach items="${products}" var="oneProduct">
-			<option value="${oneProduct}" selected="selected">${oneProduct.getName()}</option>
-			<c:set var="productId" scope="request" value="${doneProduct.getId()}"/>
-		</c:forEach>
-	</select>
+        <%--parameter "productName" will be sent to server.--%>
+        <%--Now it contains product id, change to whatever you need--%>
+        <select name="productName" id="mySelect">
+            <c:forEach items="${products}" var="oneProduct">
+                <option value="${oneProduct.id}">${oneProduct.name}</option>
+            </c:forEach>
+        </select>
 
-	
-	
-	<input type="hidden" name="oneProductList" value="${listId}" />
-	<input type="hidden" name="product" value="${x}" />
-	<input type="SUBMIT" class="b2" value="Save">
+
+
+        <input type="hidden" name="oneProductList" value="${listId}" />
+        <input type="hidden" name="product" value="${x}" />
+        <input type="SUBMIT" class="b2" value="Save">
 	</form>
 
 
